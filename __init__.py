d0ccc164eee87c37f4f14049e971aea9be324816
woaixuexi7 <115437067+woaixuexi7@users.noreply.github.com>
2024-11-18 11:10:54 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
-from .faster_rcnn_framework import FasterRCNN, FastRCNNPredictor
-from .rpn_function import AnchorsGenerator
-from .mask_rcnn import MaskRCNN
+from .group_by_aspect_ratio import GroupedBatchSampler, create_aspect_ratio_groups
+from .distributed_utils import init_distributed_mode, save_on_master, mkdir
+from .coco_eval import EvalCOCOMetric
+from .coco_utils import coco_remove_images_without_annotations, convert_coco_poly_mask, convert_to_coco_api
